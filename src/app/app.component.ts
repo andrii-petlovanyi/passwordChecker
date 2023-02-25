a64bf99c708763bc7d05c0584f01f5b91a7f5f45
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-25 19:08:58 +0200

    added(Base logic): added base logic without styles

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'passwordChecker';
+
+  password: string = '';
+  passwordStrength: string = '';
+
+  checkPasswordStrength() {
+    if (this.password.length === 0) {
+      this.passwordStrength = '';
+    } else if (this.password.length < 8) {
+      this.passwordStrength = 'weak';
+    } else if (/^[a-zA-Z]+$/.test(this.password)) {
+      this.passwordStrength = 'easy';
+    } else if (/^\d+$/.test(this.password)) {
+      this.passwordStrength = 'easy';
+    } else if (/^[!@#$%^&*]+$/.test(this.password)) {
+      this.passwordStrength = 'easy';
+    } else if (/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])/.test(this.password)) {
+      this.passwordStrength = 'strong';
+    } else if (/[a-zA-Z]/.test(this.password) && /[0-9]/.test(this.password)) {
+      this.passwordStrength = 'medium';
+    } else if (/[a-zA-Z]/.test(this.password) && /[!@#$%^&*]/.test(this.password)) {
+      this.passwordStrength = 'medium';
+    } else if (/[0-9]/.test(this.password) && /[!@#$%^&*]/.test(this.password)) {
+      this.passwordStrength = 'medium';
+    } else {
+      this.passwordStrength = 'weak';
+    }
+    console.log(this.passwordStrength)
+  }
+
+}
